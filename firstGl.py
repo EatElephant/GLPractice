4033f3c8ddec700787ec6f4e76f684078f002a62
Brian Li <brian.li@apisensor.com>
2016-10-31 19:37:59 +0800

    Initial commit of gl practice and add freeglut.dll too

---
+++ firstGl.py
@@ -0,0 +1,19 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+def drawFunc():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glRotatef(0.1,0,0.1,0)
+    glutWireTeapot(0.5)
+    #glutSolidTeapot(0.5)
+    glFlush()
+
+glutInit()
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(400,200)
+glutCreateWindow("First")
+glutDisplayFunc(drawFunc)
+glutIdleFunc(drawFunc)
+glutMainLoop()
